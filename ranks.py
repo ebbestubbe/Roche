772b9164552ab022982f936f9859d651460093f7
EbbeS <stubbeebbe@gmail.com>
2017-05-13 16:46:15 +0200

    Competing in Bronze
    added calculating script for the different rank scores.
    Beware that 'expertise gain' was missed previously.
    Refactor robot/sample storage to named tuples, for better overview
    maybe?

--- ranks.py
+++ ranks.py
@@ -7,12 +7,51 @@ Created on Sat May 13 16:14:04 2017
 
 import numpy as np
 
-#rank1
-cost1 = np.array([3,4,5])
-score1 = np.array([1,10])
+def main():
+    print(" ")
+    print("Analysis rank 1")
+    cost1 = np.array([3,4,5])
+    score1 = np.array([1,10])
+    
+    analyze(cost1,score1)
+    print(" ")
+    print("Analysis rank 2")
+    cost1 = np.array([5,6,7,8])
+    score1 = np.array([10,20,30])
+    
+    analyze(cost1,score1)
+    
+    print(" ")
+    print("Analysis rank 3")
+    cost1 = np.array([7,8,9,10,11,12,13,14])
+    score1 = np.array([30,40,50])
+    
+    analyze(cost1,score1)
+    
+    
+def analyze(cost,score):
+    print("   ")
+    avgcost = np.average(cost)
+    avgscore = np.average(score)
+    print("Cost:",cost,"Average:",avgcost)
+    print("Score:",score,"Average:",avgscore)
+    
+    
+    print(avgscore)
+    
+    mat = [[s/c for c in cost] for s in score]
+    mat = np.array(mat)
+    print(mat)
+    
+   
+    avgscoreformol = np.average(mat,axis=0)
+    
+    print("Average score for the cost:",avgscoreformol)
+    
+    avgscoreforscore = np.average(mat,axis=1)
 
-rank1 = [[s/c for c in cost] for s in score]
-rank1 = np.array(rank1)
-print(rank1)
+    print("Average cost for the score:",avgscoreforscore)
 
-rank1avgmol = average()
+    
+if __name__ == '__main__':
+    main()
