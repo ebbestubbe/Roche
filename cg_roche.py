c9a9488858d217d11ac4671dc015b72cd75f4a2a
EbbeS <stubbeebbe@gmail.com>
2017-05-15 01:18:49 +0200

    Heuristic start
    Starting helper functions,
    need decisions from laboratory and diagnosis
    
    The available_moves not actually needed until simulation

--- cg_roche.py
+++ cg_roche.py
@@ -4,16 +4,27 @@ import numpy as np
 import random
 from operator import add
 
+#Notes:
+    #If the enemy moves to laboratory, he has enough to turn in his samples
+
 # Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
 mti = {"A":0,"B":1,"C":2,"D":3,"E":4}
 itm = {v:k for k,v in mti.items()}
 def main():
 
+    '''    
+    a = [0,1,2,3,4,5]
+    b = [False,True,True]
+    c = b.index(True)
+    print(b)
+    print(a[c])
+    return
+    '''
     projects = []
     project_count = int(input())
     for i in range(project_count):
         a, b, c, d, e = [int(j) for j in input().split()]
-        projects.append([a,b,c,d,e])
+        projects.append(np.array([a,b,c,d,e]))
 
     [eprint(p) for p in projects]
     robots = []
@@ -31,13 +42,13 @@ def main():
             storage_c = int(storage_c)
             storage_d = int(storage_d)
             storage_e = int(storage_e)
-            storage = [storage_a,storage_b,storage_c,storage_d,storage_e]
+            storage = np.array([storage_a,storage_b,storage_c,storage_d,storage_e])
             expertise_a = int(expertise_a)
             expertise_b = int(expertise_b)
             expertise_c = int(expertise_c)
             expertise_d = int(expertise_d)
             expertise_e = int(expertise_e)
-            expertise = [expertise_a,expertise_b,expertise_c,expertise_d,expertise_e]
+            expertise = np.array([expertise_a,expertise_b,expertise_c,expertise_d,expertise_e])
             robots.append([target,eta,score,storage,expertise])
         #eprint(robots)
         
@@ -55,7 +66,7 @@ def main():
             cost_c = int(cost_c)
             cost_d = int(cost_d)
             cost_e = int(cost_e)
-            cost = [cost_a,cost_b,cost_c,cost_d,cost_e]
+            cost = np.array([cost_a,cost_b,cost_c,cost_d,cost_e])
             samples.append([sample_id,carried_by,rank,expertise_gain,health,cost])
         eprint("TURN",turn)
         [eprint(r) for r in robots]
@@ -65,7 +76,7 @@ def main():
         moves = availablemoves(robots,samples)
         eprint("available moves:")
         [eprint(m) for m in moves]
-            
+        ''' 
         if(turn == 0):
             print("GOTO SAMPLES")
             continue
@@ -80,7 +91,7 @@ def main():
         else:
             print(random.choice(moves))
             continue
-        '''
+        
         elif(turn>=8 and turn <=10):
             print("CONNECT",turn-8)
             continue
@@ -122,9 +133,13 @@ def main():
         
         if(robots[0][0] == "SAMPLES"):
             #if we dont have enough samples, collect a sample
-            if(len(ownedsamples)<1):
-                print("CONNECT",2)
-                continue
+            if(len(ownedsamples)<3):
+                if(all([s[2]!=2 for s in samples if s[1]==0])):    
+                    print("CONNECT",2)
+                    continue
+                else:
+                    print("CONNECT",1)
+                    continue
             else:
                 #If we have samples, go to diagnosis
                 print("GOTO DIAGNOSIS")
@@ -136,55 +151,57 @@ def main():
         if(robots[0][0] == "DIAGNOSIS"):
             #If we own samples which are unknown, identify them
             if(len(unknown_ownedsamples)>0):
-                print("CONNECT",ownedsamples[0][0])
+                
+                print("CONNECT",unknown_ownedsamples[0][0])
                 continue
             else:
                 print("GOTO MOLECULES")
                 continue
                 
             
-        eprint("ownedsamples",ownedsamples)
+        #eprint("ownedsamples",ownedsamples)
         if(robots[0][0] == "MOLECULES"):
             
-            
-            #get differences in the sample to robot storage
-            diff = np.array(ownedsamples[0][5])-np.array(robots[0][3])
-            eprint("pickup diff",diff)
-            #if we need some molecules, get them
-            if(any(_ > 0 for _ in diff)):
-                eprint("inside if(any())")
-                requesting = None
-                for i in range(len(diff)):
-                    if(diff[i]>0):
-                        eprint("i,",i)
-                        eprint("diff[i]",diff[i])
-                        eprint(itm[i])
-                        requesting = itm[i]
-                        break
-                print("CONNECT",requesting)
-            #if we dont, go to laboratory
-            else:
+            #If we have something to turn in: go to laboratory:
+            turnin = possible_turnin(robots,samples)
+            if(turnin):
                 print("GOTO LABORATORY")
                 continue
+            #If we do not, assemble a sample
+            
+            
+            neededmatrix,possible = immediately_collectable(robots,samples)
+            
+            #If any of these are possible, do them:
+            if(any(possible)):    
+                #Do the first one which is possible
+                requestmol = None
+                assemble_this = possible.index(True) #Returns the index of the first assemble-able
+                for i,m in enumerate(neededmatrix[assemble_this]):
+                    if(m>0): #fill out the molecules we need in order from A to E. Could be improved by grabbing the most critical first probably, or the ones with most overlap
+                        requestmol = itm[i]
+                        break
+                print("CONNECT ", requestmol)
+                continue
+            
+            #If none of them are possible, we should
+            #1: turn in
+            turnin = possible_turnin(robots,samples)
+            if(turnin):
+                print("GOTO LABORATORY")
+                continue
+            #2: get new samples
+                
+            
+            
         if(robots[0][0] == "LABORATORY"):
             #for all owned samples, check if they are doable
-            turnin = None
-            for o in ownedsamples:
-                eprint("sample",o)
-                eprint("owned",np.array(robots[0][3]))
-                diff = np.array(o[5])-np.array(robots[0][3])
-                diff = list(diff)
-                eprint("diff",diff)
-                if(all(i<=0 for i in diff)):
-                    eprint(diff)
-                    turnin = o[0]
-                    break
-            #if we found somehting to turn in, do it
-            if(turnin!=None):
-                print("CONNECT",turnin)
+            turnin = possible_turnin(robots,samples)
+            if(turnin):
+                print("CONNECT",turnin[0])
                 continue
             #If we dont have anything, move on
-            elif(turnin==None):
+            elif(not turnin):
                 eprint("owned samples:",ownedsamples)
                 #If we have more samples, go to molecules and finish those
                 if(len(ownedsamples)>0):
@@ -196,6 +213,46 @@ def main():
                     print("GOTO SAMPLES")
                     continue
 
+#Returns the list of possible sampleids we can turn in
+#Accounts for discounts
+#possible improvements:
+#Return list of list of samples we can turn in simultaniously(accounting for future discounts.)
+#(adjust to also return number of turns before this happens)
+def possible_turnin(robots,samples): 
+    ownedsamples = [s for s in samples if s[1]==0]
+    turnin = []
+    for s in ownedsamples:
+        needed = np.maximum(s[5] - robots[0][3] - robots[0][4],0)
+        if(all(n==0 for n in needed)):
+            turnin.append(s[0])
+    return turnin
+
+#Returns
+    #neededmatrix: list of numpy arrays: for each ownedsample: the number of each type needed
+    #possible: list of bools: for each ownedsample: True if the molecules are able to be collected
+    
+#Does not account for molecules which can be stolen, or for molecules which are being returned.
+#Only accounts for the current number of molecules in stock
+def immediately_collectable(robots,ownedsamples):
+    
+    #Molecules needed for each sample
+    neededmatrix = []
+    for o in ownedsamples:
+        needed = np.maximum(o[5] - robots[0][3] - robots[0][4],0)
+        neededmatrix.append(needed)
+    #neededmatrix: a row for each sample, describing what is needed
+    #[eprint(n) for n in neededmatrix]
+    stock = - robots[0][3] - robots[1][3] + 6 #Molecules left in stock(if there is a duplicate molecule, total will be 7, for -1 stock. special case handled)
+    #eprint("stock:",stock)
+    
+    #Figure out which are possible to do atm:
+    possible = [False]*len(neededmatrix)
+    for j,n in enumerate(neededmatrix):
+        if(all(stock[i] >= n[i] for i in range(len(n))) and np.sum(n)+np.sum(robots[0][3])<=10): #if there is enough in the stock and we can carry the mols, it is possible                    
+            #eprint("there are enough molecules in stock for ",ownedsamples[j])
+            possible[j] = True
+    return neededmatrix, possible
+                
 def availablemoves(robots,samples):
     moves = []
     if(robots[0][1] > 0):
@@ -241,7 +298,7 @@ def availablemoves(robots,samples):
         moves.append("GOTO DIAGNOSIS")
         moves.append("GOTO LABORATORY")
         #Total unavailable molecules
-        totalowned = np.array(robots[0][3]) + np.array(robots[1][3])
+        totalowned = robots[0][3] + robots[1][3]
         #If we have backpackspace, we can add molecules from those where totalowned<6
         if(sum(robots[0][3])<10):
             [moves.append("CONNECT " + str(k)) for k,v in mti.items() if totalowned[v]<6]
@@ -253,11 +310,11 @@ def availablemoves(robots,samples):
         moves.append("GOTO DIAGNOSIS")
         moves.append("GOTO MOLECULES")
         ownedsamples = [s for s in samples if s[1]==0 and s[4]!=-1]#owned and known
-        mol = np.array(robots[0][3])
-        exp = np.array(robots[0][4])
+        mol = robots[0][3]
+        exp = robots[0][4]
         
         for s in ownedsamples:
-            cost = np.array(s[5])
+            cost = s[5]
             finalcost = np.maximum(cost-exp-mol,0) #is 0 for all entries if we can complete the sample
             if(all(v == 0 for v in finalcost)):
                 moves.append("CONNECT " + str(s[0]))
